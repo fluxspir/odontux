d958acceed01b519fc5d01f8195d65e388638b66
Franck Labadille <franck@kernlog.net>
2012-09-25 21:35:18 +0200

    rm

--- tests/sh/S20-add_specialty.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash --rcfile ~/.bashodontuxrc
-
-specialties="diagnostic periodontic operative_dentistry endodontic chirurgia prosthetic orthodontic radiology pedodontic esthetic"
-
-for s in $specialties ; do
-    odontux add_specialty -n $s; 
-done    
-
--- tests/sh/S40-patient.sh
+++
@@ -1,3 +0,0 @@
-#/bin/bash --rcfile ~/.bashodontuxrc
-
-odontux add_patient -t mr -l essai -f papa --street "12 rue larralde" --city biarritz --postal_code 64200 --
