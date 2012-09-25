17dc0905d5a4b38fc673512854f5097af73399c8
Franck Labadille <franck@kernlog.net>
2012-09-25 12:53:54 +0200

    __init__

--- __init__.py
+++ __init__.py
@@ -20,6 +20,6 @@ def main(global_config, **settings):
     meta.session.configure(bind=engine)
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route(os.path.expanduser("~"), 'odontux/')
+    config.add_route('home', '/')
     config.scan()
     return config.make_wsgi_app()
