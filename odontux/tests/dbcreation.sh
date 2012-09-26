a44e87c97e4193cce4c752d0d8a36fb8671a8921
Franck Labadille <franck@kernlog.net>
2012-09-26 13:27:23 +0200

    rm

--- odontux/tests/dbcreation.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash --rcfile ~/.bashodontuxrc
-
-
-dropdb odontux_test
-sudo -u postgres createdb -O franck odontux_test
-odontux create_odontuxdb
-
-
--- odontux/tests/sh/S45-patient.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash --rcfile ~/.bashodontuxrc
-
-paid=$(odontux add_patient -t mr -l essai -f papa -s 1 -o 1 -u 1 --street "12 rue lala" --city Paris --postal_code 75015 --county "Ile de France" -b 19700101 --job travail -d $(odontux list_md --id maboule) --date 20120913)
-odontux add_medicalhistory --patient $paid -u 1 -d "cholestérol" -t "crestor" --date 20120913
-odontux add_surgeries --patient $paid -u 1  --date 20120913
-odontux add_allergies --patient $paid -u 1  --date 20120913
-
-faid=$(odontux list_family --id essai papa)
