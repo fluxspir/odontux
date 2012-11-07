3fc06efa22fc207179ddfd8d2f426b0c49e82cd7
Franck Labadille <franck@kernlog.net>
2012-11-08 00:19:14 +0100

    roles

--- odontux/constants.py
+++ odontux/constants.py
@@ -10,7 +10,14 @@ ROLE_ASSISTANT = 2
 ROLE_SECRETARY = 3
 ROLE_ADMIN = 4
 
-ROLES = ( "dentist", "nurse", "assistant", "secretary", "admin" )
+ROLES = { ROLE_DENTIST: "dentist", 
+          ROLE_NURSE: "nurse", 
+          ROLE_ASSISTANT: "assistant", 
+          ROLE_SECRETARY: "secretary", 
+          ROLE_ADMIN: "admin",
+        }
+
+ROLES_LIST = ROLES.item()
 
 #ROLES_LIST = [ 
 #               (ROLES[ROLE_DENTIST], ROLE_DENTIST), 
@@ -19,12 +26,12 @@ ROLES = ( "dentist", "nurse", "assistant", "secretary", "admin" )
 #               (ROLES[ROLE_SECRETARY], ROLE_SECRETARY), 
 #               (ROLES[ROLE_ADMIN], ROLE_ADMIN) 
 #             ]
-ROLES_LIST = [ 
-              (ROLE_DENTIST,ROLES[ROLE_DENTIST]), 
-              (ROLE_NURSE, ROLES[ROLE_NURSE]), 
-              (ROLE_ASSISTANT, ROLES[ROLE_ASSISTANT]),
-              (ROLE_SECRETARY,ROLES[ROLE_SECRETARY]), 
-              (ROLE_ADMIN, ROLES[ROLE_ADMIN]) 
-            ]
+#ROLES_LIST = [ 
+#              (ROLE_DENTIST,ROLES[ROLE_DENTIST]), 
+#              (ROLE_NURSE, ROLES[ROLE_NURSE]), 
+#              (ROLE_ASSISTANT, ROLES[ROLE_ASSISTANT]),
+#              (ROLE_SECRETARY,ROLES[ROLE_SECRETARY]), 
+#              (ROLE_ADMIN, ROLES[ROLE_ADMIN]) 
+#            ]
 
 KID_AGE = 13
