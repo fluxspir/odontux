10c49dd7ffab3a50b276e94c7533a974ed530a0b
Franck Labadille <franck@kernlog.net>
2012-11-09 14:15:37 +0100

    hours-mins

--- odontux/static/js/brothel.js
+++ odontux/static/js/brothel.js
@@ -1,20 +1,36 @@
 function clock2() 
 {
 var time = new Date();
-var secs = time.getSecondes();
-var mins = time.getMinutes();
+//var mins = time.getMinutes();
+var mins = 5
 var hours = time.getHours();
-var t = hours + ":" + mins + ":" + secs
-setInterval({document.getElementById("clock").innerHTML=t}, 1000)
+/**/
+var l = [hours, mins];
+for (k in l) { 
+  if (0 <= k && k <10) { 
+    k = "0" + k 
+  } 
 }
-
-// Timer
-
-var myVar = setInterval(function(){clock()}, 1000)
+/**/
+//if (0 <= mins && mins < 10) { mins = "0" + mins }
+//if (0 <= hours && hours < 10) { hours = "0" + hours }
+var t = hours + ":" + mins ;
+document.getElementById("clock").innerHTML=t;
+}
 
 function clock()
 {
 var d=new Date();
 var t=d.toLocaleTimeString();
 document.getElementById("clock").innerHTML=t;
+}
+
+function usermenuover(obj, username)
+{
+obj.innerHTML= username + "<br /><a href='/logout/'>Logout</a><br />"
+}
+
+function usermenuout(obj)
+{
+
 }
