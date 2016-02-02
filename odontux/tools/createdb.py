1b387ebbbbc9f70e2a645f6e1d656b109be642e6
Franck Labadille <franck@kernlog.net>
2016-02-02 16:10:00 -0200

    tells user that admin is created if needed

--- odontux/tools/createdb.py
+++ odontux/tools/createdb.py
@@ -7,7 +7,7 @@
 import sqlalchemy
 from sqlalchemy import create_engine
 import models
-from models import meta
+from models import meta, users
 
 
 class CreateOdontuxDatabaseTool():
@@ -21,3 +21,21 @@ class CreateOdontuxDatabaseTool():
     def run(self, args):
         models.init()
         meta.Base.metadata.create_all(bind=meta.session.bind.engine)
+        
+        query = meta.session.query(users.OdontuxUser)
+        query = query.all()
+        if not query:
+            # create an admin user
+            print("creating first user \"admin\"")
+            admin_user = {
+                "username": "admin",
+                "password": "please_change_password",
+                "role": 4,
+                "lastname": "admin",
+                "firstname": "admin",
+                "title": "M",
+            }
+            new_admin = users.OdontuxUser(**admin_user)
+            meta.session.add(new_admin)
+            meta.session.commit()
+
