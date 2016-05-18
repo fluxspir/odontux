cc20ae8b697f4879d9b9e44ad12b63953533a610
Franck Labadille <franck@kernlog.net>
2016-05-18 12:03:44 -0300

    rethinking of anamnesis

--- odontux/models/anamnesis.py
+++ odontux/models/anamnesis.py
@@ -5,46 +5,103 @@
 # licence BSD
 #
 from meta import Base
-import users, administration
+import users, administration, schedule
 import sqlalchemy
 import datetime
+
+try:
+    import constants
+except ImportError:
+    from odontux import constants
 
 from sqlalchemy import Table, Column, Integer, String, Date, DateTime, Boolean
 from sqlalchemy import ForeignKey
 from sqlalchemy import func
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
 
-
-class MedicalHistory(Base):
-    __tablename__ = 'medical_history'
+class Response(Base):
+    __tablename__ = 'response'
     id = Column(Integer, primary_key=True)
-    patient_id = Column(Integer, ForeignKey(administration.Patient.id))
-    dentist_id = Column(Integer, ForeignKey(users.OdontuxUser.id))
-    icd10 = Column(String, default="")
-    disease = Column(String, default="")
-    disorder = Column(String, default="")
-    habitus = Column(String, default="")
-    treatment = Column(String, default="")
-    time_stamp = Column(Date, default=func.current_date())
+    patient_id = Column(Integer, ForeignKey(administration.Patient.id),
+                                                        nullable=False)
+    patient = relationship('Patient')
+    appointment_id = Column(Integer, ForeignKey(schedule.Appointment.id),
+                                                        nullable=False)
+    type = Column(String)
 
-class PastSurgeries(Base):
-    __tablename__ = 'past_surgeries'
-    id = Column(Integer, primary_key=True)
-    patient_id = Column(Integer, ForeignKey(administration.Patient.id))
-    dentist_id = Column(Integer, ForeignKey(users.OdontuxUser.id))
-    surgery_type = Column(String, default="")
-    problem = Column(String, default="")
-    complication = Column(String, default="")
-    time_stamp = Column(Date, default=func.current_date())
+    __mapper_args__ = {
+        'polymorphic_identity': 'response',
+        'polymorphic_on': type
+    }
 
-class Allergies(Base):
-    __tablename__ = 'allergies'
+class CloseResponse(Response):
+    __tablename__ = 'close_response'
+    id = Column(Integer, ForeignKey(Response.id), primary_key=True)
+    response = Column(Integer)
+    comment = Column(String)
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'close'
+    }
+
+class OpenResponse(Response):
+    __tablename__ = 'open_response'
+    id = Column(Integer, ForeignKey(Response.id), primary_key=True)
+    response = Column(String)
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'open'
+    }
+
+class Questionnary(Base):
+    __tableaname__ = 'questionnary'
     id = Column(Integer, primary_key=True)
-    patient_id = Column(Integer, ForeignKey(administration.Patient.id))
-    dentist_id = Column(Integer, ForeignKey(users.OdontuxUser.id))
-    drug = Column(String, default="")
-    metal = Column(String, default="")
-    food = Column(String, default="")
-    other = Column(String, default="")
-    reaction = Column(String, default="")
-    time_stamp = Column(Date, default=func.current_date())
+    name = Column(String, nullable=False)
+    questions = relationship('Question', secondary=questionnary_question_table,
+                                        backref='questionnaries')
+
+class Question(Base):
+    """
+        alert_on : What we want to create an alert; could be "sim", "no", 
+        "any sentence" ; the keyword of alert_on will be search on the
+        Response.response 
+    """
+    __tablename__ = 'question'
+    id = Column(Integer, primary_key=True)
+    question = Column(String)
+    open = Column(Boolean, default=False)
+    alert_on = Column(String, default=None)
+
+#class MedicalHistory(Base):
+#    __tablename__ = 'medical_history'
+#    id = Column(Integer, primary_key=True)
+#    patient_id = Column(Integer, ForeignKey(administration.Patient.id))
+#    dentist_id = Column(Integer, ForeignKey(users.OdontuxUser.id))
+#    icd10 = Column(String, default="")
+#    disease = Column(String, default="")
+#    disorder = Column(String, default="")
+#    habitus = Column(String, default="")
+#    treatment = Column(String, default="")
+#    time_stamp = Column(Date, default=func.current_date())
+#
+#class PastSurgeries(Base):
+#    __tablename__ = 'past_surgeries'
+#    id = Column(Integer, primary_key=True)
+#    patient_id = Column(Integer, ForeignKey(administration.Patient.id))
+#    dentist_id = Column(Integer, ForeignKey(users.OdontuxUser.id))
+#    surgery_type = Column(String, default="")
+#    problem = Column(String, default="")
+#    complication = Column(String, default="")
+#    time_stamp = Column(Date, default=func.current_date())
+#
+#class Allergies(Base):
+#    __tablename__ = 'allergies'
+#    id = Column(Integer, primary_key=True)
+#    patient_id = Column(Integer, ForeignKey(administration.Patient.id))
+#    dentist_id = Column(Integer, ForeignKey(users.OdontuxUser.id))
+#    drug = Column(String, default="")
+#    metal = Column(String, default="")
+#    food = Column(String, default="")
+#    other = Column(String, default="")
+#    reaction = Column(String, default="")
+#    time_stamp = Column(Date, default=func.current_date())
