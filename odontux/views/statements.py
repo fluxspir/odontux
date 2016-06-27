d036e25dad96fb611067b69a64af61419828bfee
Franck Labadille <franck@kernlog.net>
2016-06-26 21:39:31 -0300

    FieldList of FieldList of FormField don't work ; miss an  entry.remove(index) in wtforms

--- odontux/views/statements.py
+++ odontux/views/statements.py
@@ -33,17 +33,14 @@ from odontux import constants, checks
 class QuotationGestureForm(Form):
     cotation_id = HiddenField(_('cotation_id'))
     anatomic_location = IntegerField(_('Anatomic location'), 
-                                                    [validators.Required()])
-#    specialty = SelectField(_('Specialty', coerce=int)
-#    gesture_id = SelectField(_('Technical Gesture'), coerce=int,
-#                                            [validators.Required()])
+                                                    [validators.Optional()])
     gesture_code = TextField(_('Technical gesture code'), 
                                                     [validators.Required()])
     price = DecimalField(_('Price'), [validators.Optional()])
     add_gesture = SubmitField(_('Add gesture'))
 
 class QuotationProjectForm(Form):
-    healthcare_plan_id = SelectField(_('HealthCare Plan'), coerce=int)#,
+    healthcare_plan_id = SelectField(_('HealthCare Plan'), coerce=int)
     project = FieldList(FormField(QuotationGestureForm), min_entries=1)
     add_project = SubmitField(_('Add project'))
 
@@ -92,6 +89,16 @@ def find_gesture():
 @app.route('/create_quotation?pid=<int:patient_id>&aid=<int:appointment_id>',
                                                     methods=['GET', 'POST'])
 def create_quotation(patient_id, appointment_id):
+
+    def _make_healthcare_plan_id_choices(quotation_form):
+        for quote_form in quotation_form.quotation:
+            quote_form.healthcare_plan_id.choices = [ (hcp.id, hcp.name) 
+                for hcp in meta.session.query(act.HealthCarePlan)
+                .filter(act.HealthCarePlan.id.in_(patient.healthcare_plans_id))
+                .all()
+        ]
+        return quotation_form
+ 
     authorized_roles = [ constants.ROLE_DENTIST, constants.ROLE_NURSE,
                         constants.ROLE_ASSISTANT ]
     if session['role'] not in authorized_roles:
@@ -99,12 +106,54 @@ def create_quotation(patient_id, appointment_id):
     patient = checks.get_patient(patient_id)
     appointment = checks.get_appointment(appointment_id)
     quotation_form = QuotationForm(request.form)
-    for quote_form in quotation_form.quotation:
-        quote_form.healthcare_plan_id.choices = [ (hcp.id, hcp.name) for hcp in
-            meta.session.query(act.HealthCarePlan)
-                .filter(act.HealthCarePlan.id.in_(patient.healthcare_plans_id))
-                .all()
-        ]
+    quotation_form = _make_healthcare_plan_id_choices(quotation_form)
+    quotation_gesture_form = QuotationGestureForm(request.form)
+    quotation_project_form = QuotationProjectForm(request.form)
+   
+    if request.method == 'POST':
+        if ( 'quotation-0-project-0-add_gesture' in request.form
+                                            and quotation_form.validate() ):
+            quotation_gesture_form.cotation_id =\
+                quotation_form.quotation[0].project[0].cotation_id.data
+            quotation_gesture_form.anatomic_location =\
+                quotation_form.quotation[0].project[0].anatomic_location.data
+            quotation_gesture_form.gesture_code =\
+                quotation_form.quotation[0].project[0].gesture_code.data
+            quotation_gesture_form.price =\
+                quotation_form.quotation[0].project[0].price.data
+            quotation_form.quotation[0].project.append_entry(
+                                                        quotation_gesture_form)
+            
+        elif 'quotation-0-add_project' in request.form:
+            quotation_project_form.healthcare_plan_id.choices = [ 
+                (hcp.id, hcp.name) for hcp in 
+                    meta.session.query(act.HealthCarePlan)
+                        .filter(act.HealthCarePlan.id.in_(
+                                                patient.healthcare_plans_id))
+                        .all() 
+            ]
+            quotation_project_form.healthcare_plan_id =\
+                quotation_form.quotation[0].healthcare_plan_id.data
+            new_quotation_project_form = QuotationProjectForm(request.form)
+            while quotation_form.quotation[0].project.entries:
+                entry = quotation_form.quotation[0].project.pop_entry
+                pdb.set_trace()
+            quotation_project_form.project =\
+                                            quotation_form.quotation[0].project
+
+            quotation_form.quotation.append_entry(quotation_project_form)
+            
+            while quotation_form.quotation[0].project.entries:
+                quotation_form.quotation[0].project.pop_entry()
+            
+            quotation_form.quotation[0].project.append_entry(
+                                                        quotation_gesture_form)
+
+            quotation_form = _make_healthcare_plan_id_choices(quotation_form)
+        
+        return render_template('create_quotation.html', patient=patient,
+                                    appointment=appointment,
+                                    quotation_form=quotation_form)
     
     quotation_form.validity.data = 6
     return render_template('create_quotation.html', patient=patient,
