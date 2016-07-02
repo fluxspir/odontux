fca7d69ec19a02b3496ea8cb7b0581bc04207dc7
Franck Labadille <franck@kernlog.net>
2016-07-02 11:11:10 -0300

    inactive odontuxusers can't log in

--- odontux/views/log.py
+++ odontux/views/log.py
@@ -29,56 +29,38 @@ def index():
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
+        user = ( 
+                meta.session.query(users.OdontuxUser)
+                .filter(users.OdontuxUser.username == request.form['username'],
+                        users.OdontuxUser.active.is_(True))
+                   .one_or_none()
+            )
+        if not user:
+            return redirect(url_for('logout'))
         try:
-            user = meta.session.query(users.OdontuxUser).filter\
-                   (users.OdontuxUser.username == request.form['username'])\
-                   .one()
-            try:
-                if scrypt.decrypt(b64decode(user.password), 
-                                    request.form['password'].encode("utf_8")):
-                    session['username'] = user.username
-                    session['user_id'] = user.id
-                    session['role'] = int(user.role)
-                    session['avatar_id'] = user.avatar_id
-                    session['ROLES'] = constants.ROLES.items()
-                    session['ROLE_DENTIST'] = constants.ROLE_DENTIST
-                    session['ROLE_NURSE'] = constants.ROLE_NURSE
-                    session['ROLE_ASSISTANT'] = constants.ROLE_ASSISTANT
-                    session['ROLE_SECRETARY'] = constants.ROLE_SECRETARY
-                    session['ROLE_ADMIN'] = constants.ROLE_ADMIN
-                    session['ROLE_PATIENT'] = constants.ROLE_PATIENT
-                    session['TOOTH_STATES'] = constants.TOOTH_STATES
-#                    session['UNITIES'] = [ ( id, UNIT[0] ) for id, UNIT in
-#                                            constants.UNITIES.items() ]
-#                    for unit in constants.UNITIES.items():
-#                        session[unit[1][1]] = unit[0]
-#                    
-#                    session['END_USE_REASONS'] = [ ( id, EUR[0] ) for id, EUR 
-#                                        in constants.END_USE_REASONS.items() ]
-#
-#                    session['MEDICAL_HISTORIES'] = constants.MEDICAL_HISTORIES
-#                    
-#                    for EUR in constants.END_USE_REASONS.items():
-#                        session[EUR[1][1]] = EUR[0]
-#
-#                    for anamnesis in constants.ANAMNESIS.items():
-#                        session[anamnesis[1][1]] = anamnesis[0]
-#                        #anamnesis_type = 'anamnesis_type_' + str(anamnesis[0])
-#                        #session[anamnesis_type]= anamnesis[1][0]
-#
-#                    for AR in constants.ALLERGIC_REACTIONS.items():
-#                        session[AR[1][1]] = AR[0]
-#
-                    if request.form['password'] == "please_change_password":
-                        return redirect(url_for('update_user',
-                                                body_id = user.id,
-                                                form_to_display = "gen_info"))
+            if scrypt.decrypt(b64decode(user.password), 
+                                request.form['password'].encode("utf_8")):
+                session['username'] = user.username
+                session['user_id'] = user.id
+                session['role'] = int(user.role)
+                session['avatar_id'] = user.avatar_id
+                session['ROLES'] = constants.ROLES.items()
+                session['ROLE_DENTIST'] = constants.ROLE_DENTIST
+                session['ROLE_NURSE'] = constants.ROLE_NURSE
+                session['ROLE_ASSISTANT'] = constants.ROLE_ASSISTANT
+                session['ROLE_SECRETARY'] = constants.ROLE_SECRETARY
+                session['ROLE_ADMIN'] = constants.ROLE_ADMIN
+                session['ROLE_PATIENT'] = constants.ROLE_PATIENT
+                session['TOOTH_STATES'] = constants.TOOTH_STATES
 
-                    else:
-                        return redirect(url_for('index'))
-            except scrypt.error:
-                return redirect(url_for('logout'))
-        except sqlalchemy.orm.exc.NoResultFound:
+                if request.form['password'] == "please_change_password":
+                    return redirect(url_for('update_user',
+                                            body_id = user.id,
+                                            form_to_display = "gen_info"))
+
+                else:
+                    return redirect(url_for('index'))
+        except scrypt.error:
             return redirect(url_for('logout'))
     return render_template('login.html')
 
